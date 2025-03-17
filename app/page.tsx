7f3310cb682a7c542f6f30d7eb4ecf61e4c0cd09
Gard Aanstad <gardaanstad@gmail.com>
2025-03-17 22:44:08 +0100

    patch: add target _blank rel noopener noreferrer to link

--- app/page.tsx
+++ app/page.tsx
@@ -87,7 +87,7 @@ export default function Home() {
         </p>
 
         <p className="mt-3">
-          jeg driver også <Link href="https://www.bedreoslo.no" className="font-medium">bedre oslo</Link>!
+          jeg driver også <Link href="https://www.bedreoslo.no" className="font-medium" target="_blank" rel="noopener noreferrer">bedre oslo</Link>!
         </p>
       </div>
     </article>
